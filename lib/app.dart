ffd406cf7bbf82f36f1e5612d0c21d93d3d2144a
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2024-09-24 07:37:34 +0300

    sss

--- lib/app.dart
+++ lib/app.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:salefora/core/cubite/change_language_cubit.dart';
-import 'package:salefora/core/cubite/generic_cubit_state.dart';
+import 'package:salefora/core/cubite/change_language_state.dart';
 import 'package:salefora/core/routing/app_routes.dart';
 
 import 'generated/l10n.dart';
